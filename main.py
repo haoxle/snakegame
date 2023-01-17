c37e1e00f854ace22d78b6e43108e4368691fc18
haoxle <hao-le@live.co.uk>
2023-01-17 21:28:00 +0000

    created file and variables and function and classes

---
+++ main.py
@@ -0,0 +1,44 @@
+from tkinter import *
+import random
+
+
+GAME_WIDTH = 700
+GAME_HEIGHT = 700
+SPEED = 50
+SPACE_SIZE = 30
+BODY_PARTS = 3
+SNAKE_COLOUR = "#00FF00"
+FOOD_COLOUR = "#FF0000"
+BACKGROUND_COLOUR = "#000000"
+
+
+class Snake:
+    pass
+
+
+class Food:
+    pass
+
+
+def next_turn():
+    pass
+
+
+def change_direction(new_direction):
+    pass
+
+
+def check_collisons():
+    pass
+
+
+def game_over():
+    pass
+
+
+window = Tk()
+window.title("Snake Game")
+window.resizable(False, False)
+
+
+window.mainloop()
