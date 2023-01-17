11f746c4a75626b946f4f6527136e64003162d46
haoxle <hao-le@live.co.uk>
2023-01-17 21:42:09 +0000

    created a circle food object

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from tkinter import *
 import random
 
-
 GAME_WIDTH = 700
 GAME_HEIGHT = 700
 SPEED = 50
-SPACE_SIZE = 30
+SPACE_SIZE = 20
 BODY_PARTS = 3
 SNAKE_COLOUR = "#00FF00"
 FOOD_COLOUR = "#FF0000"
@@ -13,11 +12,24 @@ BACKGROUND_COLOUR = "#000000"
 
 
 class Snake:
-    pass
+
+    def __init__(self):
+        self.body_size = BODY_PARTS
+        self.coordinates = []
+        self.squares = []
+
+        for i in range(0, BODY_PARTS):
+            self.coordinates.append([0,0])
 
 
 class Food:
-    pass
+
+    def __init__(self):
+        x = random.randint(0, (GAME_WIDTH / SPACE_SIZE) - 1) * SPACE_SIZE
+        y = random.randint(0, (GAME_HEIGHT / SPACE_SIZE) - 1) * SPACE_SIZE
+
+        self.coordinates = [x, y]
+        canvas.create_oval(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=FOOD_COLOUR, tag="food")
 
 
 def next_turn():
@@ -36,9 +48,30 @@ def game_over():
     pass
 
 
-window = Tk()
+window = Tk()  # initialising the window
 window.title("Snake Game")
 window.resizable(False, False)
 
+score = 0
+direction = 'down'
+
+label = Label(window, text="Score:{}".format(score), font=('helvetica', 40))
+label.pack()  # allows it to be added to the window
+canvas = Canvas(window, bg=BACKGROUND_COLOUR, height=GAME_HEIGHT, width=GAME_WIDTH)
+canvas.pack()
+window.update()  # renders the window
+
+window_width = window.winfo_width()
+window_height = window.winfo_height()
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+
+x = int((screen_width / 2) - (window_width / 2))
+y = int((screen_height / 2) - (window_height / 2))
+
+window.geometry(f"{window_width}x{window_height}+{x}+{y}")
+
+snake = Snake()
+food = Food()
 
 window.mainloop()
